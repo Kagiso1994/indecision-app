3cab8464a76f705888bbc2d96de583f720fa5141
Kagiso <mokautukagiso@gmail.com>
2021-01-25 23:56:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  const name = "Kagiso Sherold Mokautu";
+  return (
+    <div className="App">
+      <h1>{name}</h1> 
+      <p>Age: 26</p>
+      <p>Location: South Africa</p>
+    </div>
+  );
+}
+
+export default App;
